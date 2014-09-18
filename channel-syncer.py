127c81e5b38dfe2a4b32ddb09c9ca8a1c311b769
Ahmed I. Khalil <ahmedibrahimkhali@gmail.com>
2014-09-18 21:41:05 +0200

    Fixes #5

--- channel-syncer.py
+++ channel-syncer.py
@@ -22,21 +22,25 @@ if __name__ == "__main__":
     api = YoutubeAPI(config_content['API_KEY'])
     channel_items_array = []
 
-    # Collect Channel Items using Channel IDs
-    for channel in channels_array:
-        channel_item_array = api.list_channel_content(channel)
-        print 'Found %s Channel Items Under Channel \'%s\'' % (len(channel_item_array), channel)
-        channel_items_array.extend(channel_item_array)
+    try:
+        # Collect Channel Items using Channel IDs
+        for channel in channels_array:
+            channel_item_array = api.list_channel_content(channel)
+            print 'Found %s Channel Items Under Channel \'%s\'' % (len(channel_item_array), channel)
+            channel_items_array.extend(channel_item_array)
 
-    # Collect Channel Items Using User ID
-    for user in users_array:
-        user_channel_array = api.list_user_content(user)
-        print 'Found %s Channel Items Under User \'%s\'' % (len(user_channel_array), user)
-        channel_items_array.extend(user_channel_array)
+        # Collect Channel Items Using User ID
+        for user in users_array:
+            user_channel_array = api.list_user_content(user)
+            print 'Found %s Channel Items Under User \'%s\'' % (len(user_channel_array), user)
+            channel_items_array.extend(user_channel_array)
 
-    # Init the youtube_dl main object to begin download
-    youtubeDL = youtube_dl.YoutubeDL()
-    youtubeDL.add_default_info_extractors()
-    # Go !
-    youtubeDL.download([channel_item.item_youtube_url for channel_item in channel_items_array])
+        # Init the youtube_dl main object to begin download
+        youtubeDL = youtube_dl.YoutubeDL()
+        youtubeDL.add_default_info_extractors()
+        # Go !
+        youtubeDL.download([channel_item.item_youtube_url for channel_item in channel_items_array])
+
+    except KeyboardInterrupt:
+        print 'KeyboardInterrupt Event Occurred. Leaving ...'
 
